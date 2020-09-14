891a0be5e2bdae286ba7bdf481e0b97aeabc1e6d
Luca Scalzotto <luca@scorpiac.com>
2020-09-14 14:47:38 +0200

    Project setup

---
+++ Logger.ts
@@ -0,0 +1,55 @@
+enum LogLevel {
+  DEBUG,
+  INFO,
+  WARN,
+  ERROR,
+  OFF,
+}
+
+const logLevelNames = {
+  [LogLevel.DEBUG]: 'DEBUG',
+  [LogLevel.INFO]: 'INFO ',
+  [LogLevel.WARN]: 'WARN ',
+  [LogLevel.ERROR]: 'ERROR',
+  [LogLevel.OFF]: '',
+};
+
+export default class Logger {
+  private static minLogLevel =
+    LogLevel[process?.env?.LOG_LEVEL as keyof typeof LogLevel] ?? LogLevel.INFO;
+  private static maxNameLength = 0;
+
+  public constructor(private readonly name: string) {
+    Logger.maxNameLength = Math.max(Logger.maxNameLength, name.length);
+  }
+
+  private static padName(name: string): string {
+    return `${name}${' '.repeat(Logger.maxNameLength - name.length)}`;
+  }
+
+  public debug(message: unknown): void {
+    this.log(LogLevel.DEBUG, message);
+  }
+
+  public info(message: unknown): void {
+    this.log(LogLevel.INFO, message);
+  }
+
+  public warn(message: unknown): void {
+    this.log(LogLevel.WARN, message);
+  }
+
+  public error(message: unknown): void {
+    this.log(LogLevel.ERROR, message);
+  }
+
+  private log(level: LogLevel, message: unknown): void {
+    if (level >= Logger.minLogLevel) {
+      console.log(
+        `${new Date().toISOString()} [${logLevelNames[level]}] ${Logger.padName(
+          this.name
+        )} | ${message}`
+      );
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # tinylogger
- A no-nonsense, dependency-free, minimalistic logger
+
+A no-nonsense, dependency-free, minimalistic logger.
+
+## Usage
+
+```js
+import Logger from '@luca_scorpion/tinylogger';
+// or:
+// const Logger = require('@luca_scorpion/tinylogger');
+
+const log = new Logger('my-logger');
+log.debug('Something is happening...');
+log.info('Tell me more, tell me more.');
+log.warn('Something might be going on.');
+log.error('Panic! Red alert!');
+```
+
+The above example will print:
+
+```
+2020-09-14T12:41:21.713Z [DEBUG] my-logger | Something is happening...
+2020-09-14T12:41:21.715Z [INFO ] my-logger | Tell me more, tell me more.
+2020-09-14T12:41:21.715Z [WARN ] my-logger | Something might be going on.
+2020-09-14T12:41:21.715Z [ERROR] my-logger | Panic! Red alert!
+```
